d9ff1dbe1aa05e2ead7368597b706e9ee398a676
Lilibeth Santo <itzlilibeth@gmail.com>
2019-10-08 16:04:02 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,9 @@
 # Add your variables here
 do
-  first_number =4
+  first_number = 4
   second_number =3
-  sum = first_number + second_number
-  
+  sum = first_number+second_number
+  difference = first_number-second_number
+  product = first_number*second_number
+  quotient = first_number/second_number
 end
